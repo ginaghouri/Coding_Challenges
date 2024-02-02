24c3c0098d32051881240df1a8dac010b3a72c99
Gina <guruganasaki@gmail.com>
2024-02-02 20:06:25 +0100

    intro_sol

---
+++ Hackerrank/hackerrank_intro.py
@@ -0,0 +1,17 @@
+
+#Intro to Python
+
+#1.
+
+n= int(input().strip())
+    
+
+    # Initialize an empty string to store the result
+    result = ""
+
+    # Loop from 1 to n (inclusive)
+    for i in range(1, n + 1):
+        result += str(i)
+
+    # Print the result
+    print(result)
