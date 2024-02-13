54fb9f9a5a857c90a92cf23174321e1b429c8937
Guru GanaSaki <139076532+ginarubik@users.noreply.github.com>
2024-02-13 14:32:39 +0100

    Update evenodd_redblue_test.py

--- Codewars/evenodd_redblue_test.py
+++ Codewars/evenodd_redblue_test.py
@@ -1,24 +1,14 @@
-### TASK ONE ###
-
-"""
-This is a possible interview coding question. Let's check the task,
-implement our solution and then write tests for it:
-
-Task
-Given an integer x perform the following conditional actions:
-
+### This is a possible interview coding question.
+Task: Given an integer x perform the following conditional actions:
 If x is odd, return 'Red'
 If x is even and in the inclusive range of 2 to 5, return 'Blue'
 If x is even and in the inclusive range of 6 to 20, return 'Red'
 If x is even and greater than 20, return 'Blue'
-
 Constraint notes:
 an input integer is alway withing the range 1 to 100 inclusive
-"""
+###
 
 # EXAMPLE SOLUTION
-
-
 
 def is_within_range(num, _min, _max):
     if _min <= num <= _max:
